58314ac886461bfc6f99c1402d6cf2140994da59
knantha475@gmail.com <knantha475@gmail.com>
2026-08-12 22:44:14 +0530

    Fix complaint notifications and local development port

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,14 +1,14 @@
-// import axios from "axios";
-
-// export default axios.create({
-
-//   baseURL:
-//     "https://downtown-hotel-complaint-system.onrender.com/api",
-
-// });
-
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api",
+
+  baseURL:
+    "https://downtown-hotel-complaint-system.onrender.com/api",
+
 });
+
+// import axios from "axios";
+
+// export default axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
