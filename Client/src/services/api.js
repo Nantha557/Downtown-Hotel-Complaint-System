cb02516008299c7142f7032e0a116fb3dc406e74
knantha475@gmail.com <knantha475@gmail.com>
2026-09-01 09:32:06 +0530

    dt mail

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export default axios.create({
   baseURL:
-    "https://downtown-hotel-complaint-system.onrender.com/api",
+    "https://downtown-hotel-complaint-system-2.onrender.com/api",
 
 });
 
